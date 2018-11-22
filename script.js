303369d4982136d25403b62ec267e224a7dc43e3
Andra <andra.popa1989@gmail.com>
2018-11-22 09:27:44 +0000

    add filter effect when navigation is open for mobile devices

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-let w = window.innerWidth;
+let w = window.outerWidth;
+let navbar = document.querySelector('#navbar');
+let main_doc = document.querySelector('#main-doc');
 
 if (w < 650) {
     let navButton = document.createElement('div');
     let menu = document.createTextNode('Menu');
     navButton.appendChild(menu);
     navButton.classList.add('menuButton');
+
     document.body.appendChild(navButton);
     navButton.style.cursor = 'pointer';
     
-    let navbar = document.querySelector('#navbar');
-
+    navbar.addEventListener('click', hideNavigation);
+    
+    function hideNavigation() {
+        navbar.style.display = 'none'; 
+        navButton.textContent = 'Menu';
+    }
+    
     navButton.addEventListener('click', showNavbar);
-
+    
+    
     function showNavbar() {
+        
         if (navbar.style.display !== 'none') {
             navbar.style.display = 'none';
             navButton.textContent = 'Menu';
         } else {
             navbar.style.display = 'flex';
             navbar.style.width = '50vw';
-            navbar.style.alignItems = 'center';
             navbar.style.transition = '3s';
             navButton.textContent = 'Close';
-            
-        }     
+            main_doc.style.filter = 'blur(2px)';
+            main_doc.style.zIndex = '-5';
+        }  
     }
 } 
 
